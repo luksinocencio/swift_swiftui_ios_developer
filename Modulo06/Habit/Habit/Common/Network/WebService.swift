1b001298bc88fbb7f67dd9701ba0bb308d0260bd
luksinocencio <luksinocencio@gmail.com>
2026-07-29 23:22:34 -0300

    *

--- Modulo06/Habit/Habit/Common/Network/WebService.swift
+++ Modulo06/Habit/Habit/Common/Network/WebService.swift
@@ -5,91 +5,88 @@ import Foundation
 import Foundation
 
 enum WebService {
-  enum Endpoint: String {
-    case base = "https://habitplus-api.tiagoaguiar.co"
-    case postUser = "/users"
-  }
-  
-  enum NetworkError {
-    case badRequest
-    case notFound
-    case unauthorized
-    case internalServerError
-  }
-  
-  enum Result {
-    case success(Data)
-    case failure(NetworkError, Data?)
-  }
-  
-  private static func completeUrl(path: Endpoint) -> URLRequest? {
-    guard let url = URL(string: "\(Endpoint.base.rawValue)\(path.rawValue)") else { return nil }
-    
-    return URLRequest(url: url)
-  }
-  
-  private static func call<T: Encodable>(path: Endpoint,
-                                         body: T,
-                                         completion: @escaping (Result) -> Void) {
-    
-    guard var urlRequest = completeUrl(path: path) else { return }
-    
-    guard let jsonData = try? JSONEncoder().encode(body) else { return }
-    
-    urlRequest.httpMethod = "POST"
-    urlRequest.setValue("application/json", forHTTPHeaderField: "accept")
-    urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    urlRequest.httpBody = jsonData
-    
-    let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-      guard let data = data, error == nil else {
-        print(error)
-        completion(.failure(.internalServerError, nil))
-        return
-      }
-      
-      if let r = response as? HTTPURLResponse {
-        switch r.statusCode {
-          case 400:
-            completion(.failure(.badRequest, data))
-            break
-          case 200:
-            completion(.success(data))
-          default:
-            break
-        }
-      }
-//      
-//      
-//      
-//      print("response\n")
-//      
-//      print(response)
-//      
-//      
+    enum Endpoint: String {
+        case base = "https://habitplus-api.tiagoaguiar.co"
+        case postUser = "/users"
     }
     
-    task.resume()
-  }
-  
-  static func postUser(request: SignUpRequest) {
-    call(path: .postUser, body: request) { result in
-      switch result {
-        case .failure(let error, let data):
-          if let data = data {
-            if error == .badRequest {
-              print(String(data: data, encoding: .utf8))
-              let decoder = JSONDecoder()
-              let response = try? decoder.decode(SignUpResponse.self, from: data)
-              print(response?.detail)
+    enum NetworkError {
+        case badRequest
+        case notFound
+        case unauthorized
+        case internalServerError
+    }
+    
+    enum Result {
+        case success(Data)
+        case failure(NetworkError, Data?)
+    }
+    
+    private static func completeUrl(path: Endpoint) -> URLRequest? {
+        guard let url = URL(string: "\(Endpoint.base.rawValue)\(path.rawValue)") else { return nil }
+        
+        return URLRequest(url: url)
+    }
+    
+    private static func call<T: Encodable>(path: Endpoint,
+                                           body: T,
+                                           completion: @escaping (Result) -> Void) {
+        
+        guard var urlRequest = completeUrl(path: path) else { return }
+        
+        guard let jsonData = try? JSONEncoder().encode(body) else { return }
+        
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("application/json", forHTTPHeaderField: "accept")
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            guard let data = data, error == nil else {
+                completion(.failure(.internalServerError, nil))
+                return
             }
-          }
-          break
-        case .success(let data):
-          print(String(data: data, encoding: .utf8))
-          break
-      }
+            
+            if let r = response as? HTTPURLResponse {
+                switch r.statusCode {
+                case 400:
+                    completion(.failure(.badRequest, data))
+                    break
+                case 200:
+                    completion(.success(data))
+                default:
+                    break
+                }
+            }
+            //
+            //
+            //
+            //      print("response\n")
+            //
+            //      print(response)
+            //
+            //
+        }
+        
+        task.resume()
     }
-  }
-  
+    
+    static func postUser(request: SignUpRequest, completion: @escaping(Bool?, ErrorResponse?) -> Void) {
+        call(path: .postUser, body: request) { result in
+            switch result {
+            case .failure(let error, let data):
+                if let data = data {
+                    if error == .badRequest {
+                        let decoder = JSONDecoder()
+                        let response = try? decoder.decode(ErrorResponse.self, from: data)
+                        completion(nil, response)
+                    }
+                }
+                break
+            case .success(let data):
+                print(String(data: data, encoding: .utf8))
+                break
+            }
+        }
+    }
 }
--- Modulo06/Habit/Habit/Features/SignUp/ViewModel/SignUpViewModel.swift
+++ Modulo06/Habit/Habit/Features/SignUp/ViewModel/SignUpViewModel.swift
@@ -2,57 +2,59 @@ import SwiftUI
 import Combine
 
 class SignUpViewModel: ObservableObject {
-  @Published var fullName = ""
-  @Published var email = ""
-  @Published var password = ""
-  @Published var document = ""
-  @Published var phone = ""
-  @Published var birthday = ""
-  @Published var gender = Gender.male
-  
-  var publisher: PassthroughSubject<Bool, Never>!
-  
-  @Published var uiState: SignUpUIState = .none
-  
-  func signUp() {
-    self.uiState = .loading
+    @Published var fullName = ""
+    @Published var email = ""
+    @Published var password = ""
+    @Published var document = ""
+    @Published var phone = ""
+    @Published var birthday = ""
+    @Published var gender = Gender.male
     
-    // Pegar a String -> dd/MM/yyyy -> Date
-    let formatter = DateFormatter()
-    formatter.locale = Locale(identifier: "en_US_POSIX")
-    formatter.dateFormat = "dd/MM/yyyy"
+    var publisher: PassthroughSubject<Bool, Never>!
     
-    let dateFormatted = formatter.date(from: birthday)
+    @Published var uiState: SignUpUIState = .none
     
-    // Validar a Data
-    guard let dateFormatted = dateFormatted else {
-      self.uiState = .error("Data inválida \(birthday)")
-      return
+    func signUp() {
+        self.uiState = .loading
+        
+        // Pegar a String -> dd/MM/yyyy -> Date
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "dd/MM/yyyy"
+        
+        let dateFormatted = formatter.date(from: birthday)
+        
+        // Validar a Data
+        guard let dateFormatted = dateFormatted else {
+            self.uiState = .error("Data inválida \(birthday)")
+            return
+        }
+        
+        // Date -> yyyy-MM-dd -> String
+        formatter.dateFormat = "yyyy-MM-dd"
+        let birthday = formatter.string(from: dateFormatted)
+        
+        
+        WebService.postUser(request:
+                                SignUpRequest(
+                                    fullName: fullName,
+                                    email: email,
+                                    password: password,
+                                    document: document,
+                                    phone: phone,
+                                    birthday: birthday,
+                                    gender: gender.index
+                                )
+        ) { (successResponse, errorResponse) in
+            if let error = errorResponse {
+                self.uiState = .error(error.detail)
+            }
+        }
     }
-    
-    // Date -> yyyy-MM-dd -> String
-    formatter.dateFormat = "yyyy-MM-dd"
-    let birthday = formatter.string(from: dateFormatted)
-    
-    
-    WebService.postUser(request: SignUpRequest(fullName: fullName,
-                                               email: email,
-                                               password: password,
-                                               document: document,
-                                               phone: phone,
-                                               birthday: birthday,
-                                               gender: gender.index))
-    
-//    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-//      self.uiState = .error("Usuário já existente!")
-//      self.uiState = .success
-//      self.publisher.send(true)
-//    }
-  }
 }
 
 extension SignUpViewModel {
-  func homeView() -> some View {
-    return SignUpViewRouter.makeHomeView()
-  }
+    func homeView() -> some View {
+        return SignUpViewRouter.makeHomeView()
+    }
 }
